8caa304413ff085397334875f39122d5c405f1f1
shivamsinghydv <shivam9661446@gmail.com>
2021-09-06 13:04:27 +0530

    commit to main

--- javascript/sc.js
+++ javascript/sc.js
@@ -1,8 +1,12 @@
+function startup(){
+    
+}
 
+var algohash = sha1("Shivam")
+console.log("algohash")
 function nameInput() {
     username = prompt("Enter your name");
 }
-
 function showalert(username){
     prompt("Enter your feedback");
     alert("Thanks for your feedback " + username);
