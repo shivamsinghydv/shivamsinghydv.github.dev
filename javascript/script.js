ce2399b9eff3eb5e5d1fb6d2e8aa61b1cbec2aad
Shivam Singh <77521205+shivamsinghydv@users.noreply.github.com>
2021-12-03 22:25:55 +0530

    update script

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,6 @@ document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
     document.body.classList.toggle('dark')
     const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
     sunMoonContainer.style.setProperty('--rotation', currentRotation +180)
+    
+    location.replace('./main.html')
 })
