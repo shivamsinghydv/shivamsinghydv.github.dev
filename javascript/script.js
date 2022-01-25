6c52722f5a802da178b1597494ea03370cb80174
shivamsinghydv <shivam9661446@gmail.com>
2022-01-25 12:38:06 +0530

    dark mode added

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,11 @@ document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
 document.querySelector('.enter-button').addEventListener('click', () =>{
     window.location.href = ('./main.html')
 })
+
+const container = document.querySelector('.container')
+document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
+    document.body.classList.toggle('dark')
+})
+document.querySelector('.enter-button').addEventListener('click', () =>{
+    window.location.href = ('./index.html')
+})
