fb2bde312540282017aa0e70a66b597ffb894c64
shivamsinghydv <shivam9661446@gmail.com>
2022-02-09 17:46:42 +0530

    commit to main

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,7 @@
-const sunMoonContainer = document.querySelector('.sun-moon-container')
-
-document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
-    document.body.classList.toggle('dark')
-    const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
-    sunMoonContainer.style.setProperty('--rotation', currentRotation +180)
-})
-
-document.querySelector('.enter-button').addEventListener('click', () =>{
-    window.location.href = ('./main.html')
-})
 
 const container = document.querySelector('.container')
 document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
     document.body.classList.toggle('dark')
 })
-document.querySelector('.enter-button').addEventListener('click', () =>{
-    window.location.href = ('./index.html')
-})
+
+//write a function to add two numbers           
