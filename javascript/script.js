843c9fe02ccee7743a6f39acadada919bd3e8c7f
shivamsinghydv <shivam9661446@gmail.com>
2022-02-26 19:36:24 +0530

    menu-bar

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,13 @@
+// menu hamburger//
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+//swap themes//
 const container = document.querySelector('.container')
 document.querySelector('.theme-toggle-button').addEventListener('click', () =>{
     document.body.classList.toggle('dark')
 })
-
-//write a function to add two numbers           
