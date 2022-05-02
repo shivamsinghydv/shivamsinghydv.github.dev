ca29c73114fb06905f7b31733d41f6740fa93c16
shivamsinghydv <shivam9661446@gmail.com>
2022-05-02 11:10:14 +0530

    commit to main

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// register the service worker
+if ('serviceWorker' in navigator){
+  window.addEventListener('load',() =>{
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => {
+      console.log('registered', reg)
+    }).catch(err => {
+      console.log('failed')
+    })
+  })
+}
+
+// theme changer
 const switchElement = document.querySelector('.switch')
       switchElement.addEventListener('click', () => {
         document.body.classList.toggle('dark')
--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 self.addEventListener( 'install', event => {
-    console.log('Install event!');
+    console.log('Service worker Install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
@@ -9,3 +15,20 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event =>{
     console.log('Fetch intercepted for:', event.request.url)
 })
+
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+    '/',
+    'index.html',
+    'styles.css',
+    'assets/chatbot.png',
+    'assets/favicon.ico',
+    'assets/github.svg',
+    'assets/gmail.svg',
+    'assets/picture.jpg',
+    'assets/python.png',
+    'assets/qrcode.png',
+    'assets/spiral.png',
+    'assets/twitter.svg',
+    '/assets/undraw_creation_re_d1mi.svg'
+]
