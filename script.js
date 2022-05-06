37fdf864cf0c26634aa6e4244641fc04a3e68537
shivamsinghydv <shivam9661446@gmail.com>
2022-05-06 20:38:52 +0530

    added a couple more features

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ if ('serviceWorker' in navigator){
   })
 }
 
+//typewriter
+var i = 0;
+var txt = 'I love coding. Scroll down to browse through my projects.';
+var speed = 0.3;
+var timout = (1/speed)*10
+      
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewr").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, timout);
+ }
+}
+window.onload = typeWriter();
+
+
 // theme changer
 const switchElement = document.querySelector('.switch')
       switchElement.addEventListener('click', () => {
