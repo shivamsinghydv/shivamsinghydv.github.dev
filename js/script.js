854f3229f18e05f7d2beb2a43ec477372f0c78f7
shivamsinghydv <shivam9661446@gmail.com>
2023-04-28 21:51:16 +0530

    Fixed typing effect on landing page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-'use strict';
-function typeWriter(el) {
-    const textArray = el.innerHTML.split('');
-    el.innerHTML = '';
-    textArray.forEach((letter, i) =>
-        setTimeout(() => (el.innerHTML += letter), 95 * i)
-    );
-    setInterval(() => typeWriter(el), 8000);
-}
+// 'use strict';
+// function typeWriter(el) {
+//     const textArray = el.innerHTML.split('');
+//     el.innerHTML = '';
+//     textArray.forEach((letter, i) =>
+//         setTimeout(() => (el.innerHTML += letter), 95 * i)
+//     );
+//     setInterval(() => typeWriter(el), 8000);
+// }
 
-typeWriter(typewr);
+// typeWriter(typewr);
 
 // register the service worker
 if ('serviceWorker' in navigator){
@@ -23,17 +23,17 @@ if ('serviceWorker' in navigator){
 }
 
 
-//typewriter
-// let i = 0;
-// var txt = "I am a web-developer from India.";
-// const speed = 18;
+// typewriter
+let i = 0;
+var txt = "I am a student learning SDE.";
+const speed = 18;
       
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("typewr").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//  }
-// }
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewr").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+ }
+}
 
-// window.onload = typeWriter();
+window.onload = typeWriter();
